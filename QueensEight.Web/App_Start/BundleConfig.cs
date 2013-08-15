90cd86fc6d3a561d414a6a644ac8ac3f7383c5ba
Dennis Burton <dennis.burton@gmail.com>
2013-08-15 17:23:39 -0400

    initial upload

---
+++ QueensEight.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace QueensEight.Web
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.less"));
+
+            bundles.Add(
+                new ScriptBundle("~/bundles/vendor")
+                    .Include("~/Scripts/jquery-{version}.js")
+                    .Include("~/Scripts/bootstrap.js")
+                    .Include("~/Scripts/angular.js")
+                );
+
+        }
+
+    }
+}
---
+++ QueensEight.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QueensEight.Web.MvcApplication" Language="C#" %>
