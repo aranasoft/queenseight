f04dd9a3a96291acab1dc8074bd8ef79bac22e44
Jay Harris <jay@aranasoft.com>
2013-08-20 22:03:29 -0400

    Styling board. Styling site. Adding footer.

--- QueensEight.Web/Scripts/app/templates/board.coffee
+++ QueensEight.Web/Scripts/app/templates/board.coffee
@@ -1,9 +1,9 @@
 ﻿queensEight.boardTemplate = '''
-<div ng-repeat="rowIndex in rowIndicies">
+<div ng-repeat="rowIndex in rowIndicies" class="cell-row">
     <div ng-repeat="columnIndex in columnIndicies" class="cell">
       <div ng-show="hasQueen(rowIndex,columnIndex)" class="queen">
         <i class="icon-star"></i>
-        <img src="/Content/images/crown.png"/>
+        <img src="/Content/images/crown-alt.png"/>
       </div>
     </div>
 </div>
--- QueensEight.Web/Scripts/app/templates/board.js
+++ QueensEight.Web/Scripts/app/templates/board.js
@@ -1,4 +1,5 @@
-(function() {
-  queensEight.boardTemplate = '<div ng-repeat="rowIndex in rowIndicies">\n    <div ng-repeat="columnIndex in columnIndicies" class="cell">\n      <div ng-show="hasQueen(rowIndex,columnIndex)" class="queen">\n        <i class="icon-star"></i>\n        <img src="/Content/images/crown.png"/>\n      </div>\n    </div>\n</div>';
+﻿(function() {
+
+  queensEight.boardTemplate = '<div ng-repeat="rowIndex in rowIndicies" class="cell-row">\n    <div ng-repeat="columnIndex in columnIndicies" class="cell">\n      <div ng-show="hasQueen(rowIndex,columnIndex)" class="queen">\n        <i class="icon-star"></i>\n        <img src="/Content/images/crown-alt.png"/>\n      </div>\n    </div>\n</div>';
 
 }).call(this);
