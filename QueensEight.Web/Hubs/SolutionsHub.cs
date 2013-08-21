6e5cdb63b4b84f62e8b1cdae4a9d00f379b0ebc4
Dennis Burton <dennis.burton@gmail.com>
2013-08-21 11:36:36 -0400

    hold onto solutions until persistance is implemented

--- QueensEight.Web/Hubs/SolutionsHub.cs
+++ QueensEight.Web/Hubs/SolutionsHub.cs
@@ -17,24 +17,6 @@ namespace QueensEight.Web.Hubs
 
         private static List<Solution> solutions = new List<Solution>();
 
-        static SolutionsHub()
-        {
-            //var solution = new Solution {Hash = "12345"};
-            //var positions = new List<Position>
-            //    {
-            //        new Position {Row = 2, Column = 2},
-            //        new Position {Row = 3, Column = 0},
-            //        new Position {Row = 6, Column = 1},
-            //        new Position {Row = 7, Column = 3},
-            //        new Position {Row = 1, Column = 4},
-            //        new Position {Row = 4, Column = 5},
-            //        new Position {Row = 0, Column = 6},
-            //        new Position {Row = 5, Column = 7}
-            //    };
-            //solution.Positions = positions;
-            //solutions.Add(solution);
-        }
-
         public string FetchSolutions()
         {
             return JsonConvert.SerializeObject(solutions, serializerSettings);
@@ -52,6 +34,12 @@ namespace QueensEight.Web.Hubs
             var serializedSolution = JsonConvert.SerializeObject(solution, serializerSettings);
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<SolutionsHub>();
             context.Clients.All.SolutionAvailable(serializedSolution);
+
+            if (!solution.Positions.Any()) return;
+            if (solutions.All(s => s.Hash != solution.Hash))
+            {
+                solutions.Add(solution);
+            }
         }
 
     }
