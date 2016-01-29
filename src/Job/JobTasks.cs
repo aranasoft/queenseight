2b83e2f159a1195f610ed6f802c5eecd462990eb
Dennis Burton User <dennis.burton@gmail.com>
2016-01-29 10:59:45 -0500

    feature(job): add calls to solve a new request

--- src/Job/JobTasks.cs
+++ src/Job/JobTasks.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Linq;
 using Microsoft.Azure.WebJobs;
 using QueensEight.Processor;
 
 namespace QueensEight.Job {
     public class JobTasks {
-        public static void ProcessRequestedSolutions([ServiceBusTrigger("%requestqueuename%")] Solution theRequestedSolution)
-        {
-            Console.WriteLine(theRequestedSolution);
+        public static void ProcessRequestedSolutions([ServiceBusTrigger("%requestqueuename%")] Solution theRequestedSolution) {
+            Console.WriteLine("Solution request for " + theRequestedSolution);
+            var board = new Board();
+            board.PlaceQueensAtPositions(theRequestedSolution.Positions);
+            var queens = board.Solve();
+
+            var positions = queens.Select(queen => queen.Position).ToList();
+            var solution = new Solution {
+                Hash = Position.ListHash(positions),
+                Positions = positions,
+                RequestHash = theRequestedSolution.RequestHash
+            };
+
+            //TODO: Post solution back to WebAPI
+            Console.WriteLine("Solution: " + solution);
         }
     }
 }
